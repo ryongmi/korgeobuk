9583ce8cd537d17b8aedd41bdd92be8678829bc1
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-07-03 16:21:21 +0900

    에러 메시지 수정

--- backend/src/exception/auth.exception.ts
+++ backend/src/exception/auth.exception.ts
@@ -13,15 +13,15 @@ export class AuthException {
     return new BadRequestException('state가 존재하지 않습니다.');
   }
 
-  static authSessionNotFound(): HttpException {
-    return new UnauthorizedException('로그인이 필요합니다.');
-  }
-
   static authStateNotExist(): HttpException {
     return new ForbiddenException('state가 유효하지 않습니다.');
   }
 
-  static authNaverLoginError(): HttpException {
+  static authSessionNotFound(): HttpException {
+    return new UnauthorizedException('로그인이 필요합니다.');
+  }
+
+  static authLoginError(): HttpException {
     return new InternalServerErrorException(
       '로그인중 서버에서 에러가 발생하였습니다.',
     );
